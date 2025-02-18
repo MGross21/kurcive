d563dfb4e25c9ec1d0164dec742a4bb8a3857813
Michael Gross <mgstinger521@gmail.com>
2025-02-17 20:46:32 -0700

    Remove more gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,14 @@
-/**
- * Top-level build file for ftc_app project.
- *
- * It is extraordinarily rare that you will ever need to edit this file.
- */
-
 buildscript {
     repositories {
         mavenCentral()
         google()
     }
     dependencies {
-        // Note for FTC Teams: Do not modify this yourself.
         classpath("com.android.tools.build:gradle:8.7.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21") // Kotlin Gradle Plugin
     }
 }
 
-// This is now required because aapt2 has to be downloaded from the
-// google() repository beginning with version 3.2 of the Android Gradle Plugin
 allprojects {
     repositories {
         mavenCentral()
@@ -25,13 +16,8 @@ allprojects {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
 plugins {
-    kotlin("jvm") version "1.9.21" // Apply Kotlin plugin
-    java // Apply Java plugin
+    kotlin("jvm") version "1.9.21"
 }
 
 java {
@@ -43,5 +29,4 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21") // Kotlin Standard Library
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // Coroutines Core
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // Coroutines for Android
-    implementation("org.apache.commons:commons-math3:3.6.1") // Apache Commons Math
 }
